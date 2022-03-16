867ba1fe6f56f662ce8b59bb341e3b4bb8553e4a
Lachefdev <ebertmiriam@gmx.de>
2022-03-16 10:23:34 +0100

    updates data

--- data.js
+++ data.js
@@ -1,24 +1,24 @@
 'use strict';
 
 const products = [
-    {
+     {
         id: 1,
-        name: 'Classic chic',
-        description: 'A rubber band necklace called classic chic. With a rectangular pendant in pink.',
-        price: 22,
-        img: 'rectangle_pink.jpg'
-    }, {
-        id: 2,
         name: 'Pink passion',
         description: 'A rubber band necklace called pink passion. With a teardrop shaped pendant in pink.',
         price: 28,
         img: 'drop_pink.jpg'
     }, {
-        id: 3,
+        id: 2,
         name: 'Blue Lagoon',
         description: 'A rubber band necklace called blue lagoon. With a teardrop shaped pendant in blue.',
         price: 22,
         img: 'drop_blue_2.jpg'
+    },{
+        id: 3,
+        name: 'Classic chic',
+        description: 'A rubber band necklace called classic chic. With a rectangular pendant in pink.',
+        price: 22,
+        img: 'rectangle_pink.jpg'
     },
 ]
 
