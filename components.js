5f72ac7c72af3e5559173e503c068be96d20589e
Lachefdev <ebertmiriam@gmx.de>
2022-03-16 16:00:17 +0100

    creates shown elements

---
+++ components.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const components = {
+    piece(data, parent) {
+
+        const elCharm = document.createElement('div');
+        elCharm.className = 'column';
+        parent.append(elCharm);
+
+        let elCharmIllu = document.createElement('img');
+        elCharmIllu.src = `img/${data.img}`;
+        elCharmIllu.className = 'charmIllu';
+        elCharm.append(elCharmIllu);
+
+        let elBez = document.createElement('h2');
+        elBez.innerHTML = data.name;
+        elCharm.append(elBez);
+
+        let elDes = document.createElement('p');
+        elDes.className = 'description';
+        elDes.innerHTML = data.description;
+        elCharm.append(elDes);
+
+        let elPrice = document.createElement('div');
+        elPrice.className = 'price';
+        elPrice.innerHTML = `${data.price} Euro`;
+        elCharm.append(elPrice);
+
+        let elKaufBtn = document.createElement('button');
+        elKaufBtn.className = 'kaufenBtn'
+        elKaufBtn.innerHTML = 'Add to basket';
+        elKaufBtn.addEventListener('click', () => addToBasket(data));
+        elCharm.append(elKaufBtn);
+
+    },
+
+    bagPiece(id, amount, parent){
+
+        const product = products.find(product => product.id == id);
+
+        const elPickedCharm = document.createElement('div');
+        elPickedCharm.className = 'cartPiece';
+        parent.append(elPickedCharm);
+
+        const elPickedIllu = document.createElement('img');
+        elPickedIllu.src = `img/${product.img}`;
+        elPickedIllu.className = 'pickedIllu';
+        elPickedCharm.append(elPickedIllu);
+
+        const elPickedBez = document.createElement('h2');
+        elPickedBez.innerHTML = product.name;
+        elPickedCharm.append(elPickedBez);
+
+    },
+
+
+
+}
