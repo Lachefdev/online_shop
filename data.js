f41a6286dd38329781648be4a9de178488f20629
Lachefdev <ebertmiriam@gmx.de>
2022-03-17 18:44:13 +0100

    completes data for now

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ const products = [
         id: 1,
         name: 'Pink passion',
         description: 'A rubber band necklace called pink passion. With a teardrop shaped pendant in pink.',
-        price: 28,
+        price: 22,
         img: 'drop_pink.jpg'
     }, {
         id: 2,
@@ -17,9 +17,28 @@ const products = [
         id: 3,
         name: 'Classic chic',
         description: 'A rubber band necklace called classic chic. With a rectangular pendant in pink.',
-        price: 22,
+        price: 24,
         img: 'rectangle_pink.jpg'
-    },
+    },{
+        id: 4,
+        name: 'Dark night',
+        description: 'A necklace out of metal with a screw top AND a dark, round pendant.',
+        price: 27,
+        img: 'round_black_3.jpg'
+    },{
+        id: 5,
+        name: 'Dancing queen',
+        description: 'A necklace out of metal with a screw top AND a rectangular pendant in esmerald green.',
+        price: 28,
+        img: 'rectangle_green.jpg'
+    },{
+        id: 6,
+        name: 'Party princess',
+        description: 'A necklace out of metal with a screw top AND a purple pendant in the form of a diamond.',
+        price: 32,
+        img: 'diamond_purple_4.jpg'
+    }
+
 ]
 
 let warenkorb = {};
